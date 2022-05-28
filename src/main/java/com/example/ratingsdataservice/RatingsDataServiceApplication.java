737a170237ecd1d853e4082d3b47e6fa28b71932
007amits <amitshukla5502@gmail.com>
2022-05-29 02:42:53 +0530

    Reset the project

--- src/main/java/com/example/ratingsdataservice/RatingsDataServiceApplication.java
+++ src/main/java/com/example/ratingsdataservice/RatingsDataServiceApplication.java
@@ -1,4 +1,4 @@
-package com.amit.ratingsdataservice;
+package com.example.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/ratingsdataservice/RatingsDataServiceApplicationTests.java
+++ src/test/java/com/example/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amit.ratingsdataservice;
+package com.example.ratingsdataservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
